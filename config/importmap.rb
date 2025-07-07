d69ce1d73837877ebef581abee5e8a08c8811aa3
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-07-08 00:27:17 +0530

    Pin travellers in importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 pin "search"
-
+pin "travellers"
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
