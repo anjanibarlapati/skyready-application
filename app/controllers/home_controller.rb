ae622c2e94270cd59338b9dc84ef15481c5ec03d
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-07 10:45:21 +0530

    Add controller for filtering flights data from file based on user inputs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @flights = []
+    @searched = false
+
+    if params[:source].present? && params[:destination].present?
+      @searched = true
+      @flights = FlightDataReader.search(params[:source], params[:destination])
+    end
   end
 end
