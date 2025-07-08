d25ae173e84156b3c23497cc8a2b1a2d2752c82e
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-08 11:38:14 +0530

    Update home controller to take class type as param

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,13 @@ class HomeController < ApplicationController
 
     if params[:source].present? && params[:destination].present?
       @searched = true
+
       @flights = FlightDataReader.search(
         params[:source],
         params[:destination],
         params[:departure_date].presence,
-        params[:travellers_count] || 1
+        params[:travellers_count] || 1,
+        params[:class_type].presence || "Economic"
       )
     end
   end
