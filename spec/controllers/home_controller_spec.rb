f020ca0f7a1910fe4151c525e096d1b088d67101
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-08 11:37:40 +0530

    Update tests for controller to take class type as param

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe HomeController, type: :controller do
     end
 
     context "when source and destination are provided without departure_date or travellers_count" do
-      it "sets searched to true and fetches flights with default travellers_count" do
+      it "sets searched to true and fetches flights with default travellers_count and class_type" do
         fake_flights = [
           { flight_number: "AI101", source: "Delhi", destination: "Mumbai" }
         ]
 
         expect(FlightDataReader).to receive(:search)
-          .with("Delhi", "Mumbai", nil, 1)
+          .with("Delhi", "Mumbai", nil, 1, "Economic")
           .and_return(fake_flights)
 
         get :index, params: { source: "Delhi", destination: "Mumbai" }
@@ -29,13 +29,13 @@ RSpec.describe HomeController, type: :controller do
     end
 
     context "when source, destination, and departure_date are provided without travellers_count" do
-      it "sets searched to true and fetches flights with date and default travellers_count" do
+      it "sets searched to true and fetches flights with date, default travellers_count and class_type" do
         fake_flights = [
           { flight_number: "AI102", source: "Delhi", destination: "Mumbai", departure_date: "2025-07-07" }
         ]
 
         expect(FlightDataReader).to receive(:search)
-          .with("Delhi", "Mumbai", "2025-07-07", 1)
+          .with("Delhi", "Mumbai", "2025-07-07", 1, "Economic")
           .and_return(fake_flights)
 
         get :index, params: { source: "Delhi", destination: "Mumbai", departure_date: "2025-07-07" }
@@ -45,17 +45,23 @@ RSpec.describe HomeController, type: :controller do
       end
     end
 
-    context "when source, destination, departure_date and travellers_count are provided" do
+    context "when all params are provided" do
       it "sets searched to true and fetches flights with all params" do
         fake_flights = [
           { flight_number: "AI103", source: "Delhi", destination: "Mumbai", departure_date: "2025-07-07" }
         ]
 
         expect(FlightDataReader).to receive(:search)
-          .with("Delhi", "Mumbai", "2025-07-07", "3")
+          .with("Delhi", "Mumbai", "2025-07-07", "3", "First Class")
           .and_return(fake_flights)
 
-        get :index, params: { source: "Delhi", destination: "Mumbai", departure_date: "2025-07-07", travellers_count: "3" }
+        get :index, params: {
+          source: "Delhi",
+          destination: "Mumbai",
+          departure_date: "2025-07-07",
+          travellers_count: 3,
+          class_type: "First Class"
+        }
 
         expect(assigns(:searched)).to eq(true)
         expect(assigns(:flights)).to eq(fake_flights)
