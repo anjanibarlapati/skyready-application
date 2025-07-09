a48873684c1d0309483a7a765d53ccfe9c582f4e
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-09 10:16:11 +0530

    Add styles when increment and decrement buttons disabled

--- app/javascript/travellers.js
+++ app/javascript/travellers.js
@@ -1,13 +1,23 @@
-document.addEventListener("turbo:load", function() {
+document.addEventListener("turbo:load", function () {
   const incrementBtn = document.querySelector(".counter-btn:last-of-type");
   const decrementBtn = document.querySelector(".counter-btn:first-of-type");
   const input = document.getElementById("travellers_count");
+
+  function updateButtonState() {
+    const count = parseInt(input.value);
+    decrementBtn.disabled = count <= 1;
+    incrementBtn.disabled = count >= 9;
+
+    decrementBtn.classList.toggle("disabled", decrementBtn.disabled);
+    incrementBtn.classList.toggle("disabled", incrementBtn.disabled);
+  }
 
   if (incrementBtn && decrementBtn && input) {
     incrementBtn.addEventListener("click", () => {
       let count = parseInt(input.value);
       if (count < 9) {
         input.value = count + 1;
+        updateButtonState();
       }
     });
 
@@ -15,7 +25,10 @@ document.addEventListener("turbo:load", function() {
       let count = parseInt(input.value);
       if (count > 1) {
         input.value = count - 1;
+        updateButtonState();
       }
     });
+
+    updateButtonState();
   }
 });
