6bf401a29b546d779aaf39e5e27da7ff07daf8b5
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-07-14 11:41:22 +0530

    Implement fetch cities controller

---
+++ app/controllers/api/v1/cities_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class CitiesController < Api::BaseController
+      def index
+        cities = []
+
+        File.foreach(file_path) do |line|
+          next if line.strip.empty?
+          data = line.strip.split(',')
+          next if data.size < 4
+          from_city = data[2]
+          to_city = data[3]
+          cities << from_city
+          cities << to_city
+        end
+
+        unique_cities = cities.uniq.sort
+
+        render json: unique_cities
+      end
+
+      def file_path
+        Rails.root.join("app/assets/data/data.txt")
+      end
+    end
+  end
+end
