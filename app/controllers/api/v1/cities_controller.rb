4323cdf653d7cbdb2ad79d8d07ff34fadda6cecd
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-07-17 16:54:12 +0530

    Implement unique cities fetching controller from database

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -2,25 +2,12 @@ module Api
   module V1
     class CitiesController < Api::BaseController
       def index
-        cities = []
+        from_cities = Flight.distinct.pluck(:source)
+        to_cities = Flight.distinct.pluck(:destination)
 
-        File.foreach(file_path) do |line|
-          next if line.strip.empty?
-          data = line.strip.split(",")
-          next if data.size < 4
-          from_city = data[2]
-          to_city = data[3]
-          cities << from_city
-          cities << to_city
-        end
-
-        unique_cities = cities.uniq.sort
+        unique_cities = (from_cities + to_cities).uniq.sort
 
         render json: unique_cities
-      end
-
-      def file_path
-        Rails.root.join("app/assets/data/data.txt")
       end
     end
   end
