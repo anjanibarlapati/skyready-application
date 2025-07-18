2e02f5483280ccfc5846ce15b2efb1a670b44f84
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-07-18 12:41:47 +0530

    Implement booking flights service by handling simultaneous booking

--- app/services/flight_service.rb
+++ app/services/flight_service.rb
@@ -98,4 +98,32 @@ class FlightService
       seats_available: seats_available
     }
   end
+
+
+  def self.reduce_seats(flight_number, departure_datetime, class_type, travellers_count)
+    return false if travellers_count <= 0
+
+    Flight.transaction do
+      flight = Flight.find_by(
+        flight_number: flight_number,
+        departure_datetime: departure_datetime
+      )
+      return false unless flight
+
+      seat = flight.flight_seats.find_by(class_type: class_type)
+      return false unless seat
+
+      seat.with_lock do
+        if seat.available_seats >= travellers_count
+          seat.available_seats -= travellers_count
+          seat.save!
+          return true
+        else
+          return false
+        end
+      end
+    end
+  rescue => e
+    false
+  end
 end
