b69527d470bf0a5e142ca31d97ff811d6f7be241
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-07-22 16:36:42 +0530

    Remove unnecessary migrations and models

--- app/models/flight.rb
+++
@@ -1,6 +0,0 @@
-class Flight < ApplicationRecord
-  belongs_to :airline
-  has_many :flight_seats, dependent: :destroy
-
-  validates :flight_number, :source, :destination, :departure_datetime, :arrival_datetime, presence: true
-end
