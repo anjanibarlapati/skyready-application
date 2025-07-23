54a33aabd1b960b4dc22f9b5de794a391e3bf19e
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-23 13:56:17 +0530

    Update tests for updated flight route model

--- spec/models/flight_route_spec.rb
+++ spec/models/flight_route_spec.rb
@@ -1,51 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe FlightRoute, type: :model do
-  describe 'associations' do
-    it {
-      should belong_to(:airline)
-        .with_foreign_key(:airline_name)
-        .class_name('Airline')
-    }
+  let(:airline) { Airline.create!(name: "TestAir") }
+
+  subject do
+    described_class.new(
+      airline: airline,
+      source: "DEL",
+      destination: "BOM"
+    )
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:flight_number) }
-    it { should validate_presence_of(:airline_name) }
-    it { should validate_presence_of(:source) }
-    it { should validate_presence_of(:destination) }
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
 
-    context 'uniqueness validation' do
-      before do
-        create(:airline, name: "Indigo")
-        create(:flight_route,
-          flight_number: "AI202",
-          source: "Delhi",
-          destination: "Mumbai",
-          airline_name: "Indigo"
-        )
-      end
+    it "is invalid without a source" do
+      subject.source = nil
+      expect(subject).not_to be_valid
+      expect(subject.errors[:source]).to include("can't be blank")
+    end
 
-      it 'is invalid when flight_number with same source and destination already exists' do
-        duplicate = build(:flight_route,
-          flight_number: "AI202",
-          source: "Delhi",
-          destination: "Mumbai",
-          airline_name: "Indigo"
-        )
-        expect(duplicate).not_to be_valid
-        expect(duplicate.errors[:flight_number]).to include("with this source and destination already exists")
-      end
+    it "is invalid without a destination" do
+      subject.destination = nil
+      expect(subject).not_to be_valid
+      expect(subject.errors[:destination]).to include("can't be blank")
+    end
 
-      it 'is valid when flight_number is same but source or destination is different' do
-        different_route = build(:flight_route,
-          flight_number: "AI202",
-          source: "Chennai",
-          destination: "Mumbai",
-          airline_name: "Indigo"
-        )
-        expect(different_route).to be_valid
-      end
+    it "is invalid with duplicate airline + source + destination" do
+      described_class.create!(
+        airline: airline,
+        source: "DEL",
+        destination: "BOM"
+      )
+
+      duplicate = described_class.new(
+        airline: airline,
+        source: "DEL",
+        destination: "BOM"
+      )
+
+      expect(duplicate).not_to be_valid
+      expect(duplicate.errors[:airline_id]).to include("already has a route between this source and destination")
+    end
+
+    it "allows same source-destination for different airlines" do
+      another_airline = Airline.create!(name: "OtherAir")
+      described_class.create!(
+        airline: airline,
+        source: "DEL",
+        destination: "BOM"
+      )
+
+      new_route = described_class.new(
+        airline: another_airline,
+        source: "DEL",
+        destination: "BOM"
+      )
+
+      expect(new_route).to be_valid
+    end
+  end
+
+  describe "associations" do
+    it "belongs to airline" do
+      assoc = described_class.reflect_on_association(:airline)
+      expect(assoc.macro).to eq(:belongs_to)
+    end
+
+    it "has many flights with dependent: :destroy" do
+      assoc = described_class.reflect_on_association(:flights)
+      expect(assoc.macro).to eq(:has_many)
+      expect(assoc.options[:dependent]).to eq(:destroy)
     end
   end
 end
