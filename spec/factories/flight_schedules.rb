99ad397d90a1c4e7c26a446700d6555f28812e4b
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-07-24 00:16:13 +0530

    Modify time in the flight_schedule factory

--- spec/factories/flight_schedules.rb
+++ spec/factories/flight_schedules.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :flight_schedule do
     association :flight
-    departure_time { "10:00" }
-    arrival_time { "12:00" }
+    departure_time { "10:00:00" }
+    arrival_time { "12:00:00" }
     start_date { Date.today }
     end_date { Date.today + 30 }
     recurring { true }
