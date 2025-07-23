fd4fcca24f75336c74dc780700f24099a195ddfd
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-23 14:02:04 +0530

    Update flight seat model

--- app/models/flight_seat.rb
+++ app/models/flight_seat.rb
@@ -3,7 +3,16 @@ class FlightSeat < ApplicationRecord
 
   CLASS_TYPES = [ "Economy", "Second Class", "First Class" ].freeze
 
-  validates :class_type, presence: true, inclusion: { in: CLASS_TYPES }
-  validates :total_seats, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :base_price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :class_type,
+    presence: true,
+    uniqueness: { scope: :flight_schedule_id },
+    inclusion: { in: CLASS_TYPES }
+
+  validates :total_seats,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 0 }
+
+  validates :base_price,
+    presence: true,
+    numericality: { greater_than_or_equal_to: 0 }
 end
