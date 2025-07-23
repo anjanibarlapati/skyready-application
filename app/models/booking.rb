da460771139242df55567a7b1b3bc4bf23d30c7e
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-23 14:02:46 +0530

    Update booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,6 @@ class Booking < ApplicationRecord
   CLASS_TYPES = [ "Economy", "Second Class", "First Class" ].freeze
 
   validates :flight_date, :class_type, :available_seats, presence: true
-  validates :class_type, inclusion: { in: CLASS_TYPES }
-  validates :available_seats, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :class_type, uniqueness: { scope: [ :flight_schedule_id, :flight_date ] }, inclusion: { in: CLASS_TYPES }
+  validates :available_seats, numericality: { greater_than_or_equal_to: 0 }
 end
