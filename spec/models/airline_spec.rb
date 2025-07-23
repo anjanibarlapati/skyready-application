417ba4bdb085a6dce3990a50ff4826ee9e7b903a
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-23 13:55:26 +0530

    Update tests for updated airlines model

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -1,25 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
-  describe 'associations' do
-    it { should have_many(:flight_routes).with_foreign_key(:airline_name).dependent(:destroy) }
-  end
+  subject { described_class.new(name: "TestAir") }
 
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
+  describe "validations" do
+    it "is valid with a unique name" do
+      expect(subject).to be_valid
+    end
 
-    it 'validates uniqueness of name' do
-      create(:airline, name: "IndiGoo")
-      duplicate = build(:airline, name: "IndiGoo")
+    it "is invalid without a name" do
+      subject.name = nil
+      expect(subject).not_to be_valid
+      expect(subject.errors[:name]).to include("can't be blank")
+    end
 
+    it "is invalid with a duplicate name" do
+      described_class.create!(name: "TestAir")
+      duplicate = described_class.new(name: "TestAir")
       expect(duplicate).not_to be_valid
       expect(duplicate.errors[:name]).to include("has already been taken")
     end
   end
 
-  describe 'primary key' do
-    it 'uses name as the primary key' do
-      expect(described_class.primary_key).to eq('name')
+  describe "associations" do
+    it "has many flight_routes" do
+      assoc = described_class.reflect_on_association(:flight_routes)
+      expect(assoc.macro).to eq(:has_many)
+      expect(assoc.options[:dependent]).to eq(:destroy)
     end
   end
 end
