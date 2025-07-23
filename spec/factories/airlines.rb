8e4de287e49258b2fed441621235f18475a305c0
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-23 14:34:35 +0530

    Update airlines and flight routes factories

--- spec/factories/airlines.rb
+++ spec/factories/airlines.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :airline do
-    sequence(:name) { |n| "Airline#{n}" }
+    name { "Test Airline" }
   end
 end
--- spec/factories/flight_routes.rb
+++ spec/factories/flight_routes.rb
@@ -1,10 +1,7 @@
 FactoryBot.define do
   factory :flight_route do
-    sequence(:flight_number) { |n| "AI#{n}" }
-    source { "Delhi" }
-    destination { "Mumbai" }
-
     association :airline
-    airline_name { airline.name }
+    source { "Mumbai" }
+    destination { "Delhi" }
   end
 end
