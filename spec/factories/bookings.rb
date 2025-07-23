66f83532c354e55d802ee5ed098bced42031cfde
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-23 21:20:34 +0530

    Add test bookings data

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :booking do
     association :flight_schedule
-    class_type { "Economy" }
     flight_date { Date.today }
-    available_seats { 20 }
+    class_type { "Economy" }
+    available_seats { 90 }
   end
 end
