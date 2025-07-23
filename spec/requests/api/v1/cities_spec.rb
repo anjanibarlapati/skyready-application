2ce8e95b6e5a296d925ba9d7f40a66fea3f5e9a2
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-23 14:33:58 +0530

    Update tests for fetching cities request

--- spec/requests/api/v1/cities_spec.rb
+++ spec/requests/api/v1/cities_spec.rb
@@ -2,21 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Cities", type: :request do
   describe "GET /api/v1/cities" do
-   before do
-      Booking.destroy_all
-      FlightSeat.destroy_all
-      FlightSchedule.destroy_all
-      FlightRoute.destroy_all
-      Airline.destroy_all
+    before do
+      [Booking, FlightSeat, FlightSchedule, FlightRoute, Airline].each(&:delete_all)
     end
 
     it "returns an empty array if there are no routes" do
-      FlightRoute.delete_all
-
       get "/api/v1/cities"
-
       expect(response).to have_http_status(:success)
-
       json = JSON.parse(response.body)
       expect(json).to eq([])
     end
@@ -27,11 +19,9 @@ RSpec.describe "Api::V1::Cities", type: :request do
       create(:flight_route, airline:, source: "Goa", destination: "Bengaluru")
 
       get "/api/v1/cities"
-
       expect(response).to have_http_status(:success)
-
       json = JSON.parse(response.body)
-      expect(json).to eq([ "Bengaluru", "Delhi", "Goa", "Mumbai" ])
+      expect(json).to eq(%w[Bengaluru Delhi Goa Mumbai])
     end
   end
 end
