b17929b8fdc6db04c7e45407e2a57783e61a0284
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-23 21:20:01 +0530

    Add test flight schedule data

--- spec/factories/flight_schedules.rb
+++ spec/factories/flight_schedules.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :flight_schedule do
-    association :flight_route
+    association :flight
     departure_time { "10:00" }
-    arrival_time   { "12:00" }
-    start_date     { Date.today }
-    end_date       { Date.today + 10 }
-    recurring      { true }
-    days_of_week   { [ Date.today.wday ] }
+    arrival_time { "12:00" }
+    start_date { Date.today }
+    end_date { Date.today + 30 }
+    recurring { true }
   end
 end
