f6e21f016db88e746b975c9a41c8bac93d5e91af
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-07-29 15:57:56 +0530

    Modify the order of return statements

--- app/controllers/api/v1/flights_search_controller.rb
+++ app/controllers/api/v1/flights_search_controller.rb
@@ -13,8 +13,8 @@ module Api
 
         result = FlightSearchService.search(source, destination, parsed_date, travellers_count, class_type)
         return render_error(:not_found, "Flights are not operating between given source and destination") unless result[:found_route]
-        return render_error(:conflict, "Flights with #{class_type.downcase} are not available between the selected source and destination on selected date") unless result[:found_class_type]
         return render_error(:conflict, "No flights available on the selected date") unless result[:found_date]
+        return render_error(:conflict, "Flights with #{class_type.downcase} are not available between the selected source and destination on selected date") unless result[:found_class_type]
         return render_error(:conflict, "No seats available for #{class_type.downcase} class on selected date") unless result[:seats_available]
 
         render json: { flights: result[:flights] }, status: :ok
